3499af9523d4751d1c4b030c4e9cf0f7bfbc5313
vinayak149 <vinayak.sharma7149@gmail.com>
2022-07-25 17:53:59 +0530

    fixed car.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,8 @@ return(
 	<div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
       <div>
-      <h2>{props.name}</h2>
-      <p> {props.email} </p>
+      <h2>{name}</h2>
+      <p> {email} </p>
       </div>
     </div>  
 	);
